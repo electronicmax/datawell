2859d4914e0d2eb269a388a68f8a3f102cb044cf
electronic Max <max@hip.cat>
2013-10-21 18:48:57 +0100

    bloop

--- html/js/mockup-data.js
+++ html/js/mockup-data.js
@@ -2,12 +2,28 @@ angular
    .module('datawell')
    .factory('mockupdata', function() {
    		return {
+   			people: [
+    			{
+	   			   name:"Max",
+	   			   img:'imgs/foobar.png',
+   			  },
+		      {
+      			   name:"James",
+	   			   img:'imgs/foobar.png',
+   			  }
+   			],
    			today:[
    			   {
    			      type:'reminder',
    			      app :'heart',
-   			      who: "Max",
+   			      for_whom: "Max",
    			      message:'Schedule a heart checkup with Dr. Robinson'
+   			   },
+   			   {
+   			      type:'reminder',
+   			      app :'heart',
+   			      for_whom: "Johnny",
+   			      message:'Schedule an MMR vaccination for Johnny'
    			   }
    			]
    		};
--- html/js/mockup.js
+++ html/js/mockup.js
@@ -2,6 +2,7 @@ angular
   .module('datawell', ['ui'])
   .controller('mockup', function($scope, mockupdata) {
  	$scope.fakedata = mockupdata;
+ 	$scope.people = mockupdata.people;
   }).directive('reminder', function() {
 	return {
 		restrict:'E',
@@ -9,6 +10,16 @@ angular
   		templateUrl:'templates/reminder.html',
 		controller:function($scope) {
 			console.log("model ", $scope.model);
+			console.log('model.type === reminder', $scope.model.type == 'reminder');
+			return;
+  		}
+  	};
+}).directive('person', function() { 
+	return {
+		restrict:'E',
+		scope:{model:'=model'},
+  		templateUrl:'templates/person.html',
+		controller:function($scope) {
 			return;
   		}
   	};
